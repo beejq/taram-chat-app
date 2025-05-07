73b2762d1afd90720ec8bd6047f22af1a5c44a81
Bien <bienjulius0@gmail.com>
2025-05-07 19:36:07 +0800

    Add new features (Update Profile, Message Function)

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -99,7 +99,20 @@ export const updateProfile = async (req, res) => {
 
         const uploadResponse = await cloudinary.uploader.upload(profilePic);
         const updatedUser = await User.findByIdAndUpdate(userId, {profilePic:uploadResponse.secure_url})
+
+        res.status(200).json(updateUser);
+
     } catch (error) {
-        
+        console.log("Error in update profile:", error);
+        res.status(500).json({message: "Internal server error"});   
+    }
+}
+
+export const checkAuth = (req, res) => {
+    try {
+        res.status(200).json(req.user);
+    } catch (error) {
+        console.log("Error in checkAuth controller", error.message);
+        res.status(500).json({ message: "Internal Server Error" });
     }
 }
