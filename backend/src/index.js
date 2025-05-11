c96c7cb56d301441ec1cd95015909f2fc4c2f3a9
Bien <bienjulius0@gmail.com>
2025-05-11 13:21:07 +0800

    Debug again

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,9 +9,12 @@ import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-import { app, server } from "./lib/socket.js";
+import { createServer } from "./lib/socket.js";
 
 dotenv.config();
+
+const app = express();
+const server = createServer(app);
 
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
