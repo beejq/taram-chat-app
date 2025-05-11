ad1b92008ba0c6c7c10019536d9b06f2351ddca6
Bien <bienjulius0@gmail.com>
2025-05-11 11:59:10 +0800

    Fix #2

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,4 +1,4 @@
-import express from "express"
+import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
 import { getUsersForSidebar, sendMessage } from "../controllers/message.controller.js";
 import { getMessages } from "../controllers/message.controller.js";
@@ -6,9 +6,9 @@ import { getMessages } from "../controllers/message.controller.js";
 const router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSidebar); //Getting all registered users
-router.get("/:id", protectRoute, getMessages); //Getting all messages
 
 router.post("/send/:id", protectRoute, sendMessage); //Sending message to a specific user
+router.get("/:id", protectRoute, getMessages); //Getting all messages
 
 
 export default router;
